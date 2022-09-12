5aa8916f19dcd38b76adfe8039188368181b15e2
Borodin Alexandr <bvachtang@gmail.com>
2022-09-12 12:36:40 +0300

    сделать проектную 13

---
+++ controllers/cards.js
@@ -0,0 +1,80 @@
+const Card = require('../models/card');
+
+module.exports.getCards = (req, res) => {
+  Card.find({})
+    .then((cards) => res.send({ data: cards }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: err.message });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
+};
+
+module.exports.deleteCard = (req, res) => {
+  Card.findByIdAndDelete(req.params.id)
+    .then((card) => res.send({ data: card }))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(404).send({ message: err.message });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
+};
+
+module.exports.createCard = (req, res) => {
+  const { name, link, owner } = req.body;
+  Card.create({ name, link, owner })
+    .then((card) => {
+      res.send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: err.message });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
+};
+
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { new: true },
+  )
+    .then((card) => {
+      res.send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: err.message });
+      } else if (err.name === 'CastError') {
+        res.status(404).send({ message: err.message });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
+};
+
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { new: true },
+  )
+    .then((card) => {
+      res.send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: err.message });
+      } else if (err.name === 'CastError') {
+        res.status(404).send({ message: err.message });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
+};
---
+++ controllers/users.js
@@ -0,0 +1,74 @@
+const User = require('../models/user');
+
+module.exports.getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: err.message });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
+};
+
+module.exports.getUser = (req, res) => {
+  User.findById(req.params.userId)
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(404).send({ message: err.message });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
+};
+
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: err.message });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
+};
+
+module.exports.changeUserInfo = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(req.user._id, { name, about })
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: err.message });
+      } else if (err.name === 'CastError') {
+        res.status(404).send({ message: err.message });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
+};
+
+module.exports.changeUserAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(req.user._id, { avatar })
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: err.message });
+      } else if (err.name === 'CastError') {
+        res.status(404).send({ message: err.message });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
+};

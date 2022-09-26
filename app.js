4656781dfbd3aab92671b5099fd1aafae3cb55a2
Borodin Alexandr <bvachtang@gmail.com>
2022-09-26 21:15:14 +0300

    поправить отправку нового пользователя

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.post('/signin', celebrate({
   body: Joi.object().keys({
     body: Joi.object().keys({
       email: Joi.string().required().email(),
-      password: Joi.string().required().min(8),
+      password: Joi.string().required().min(8).uri(),
     }),
   }),
 }), login);
